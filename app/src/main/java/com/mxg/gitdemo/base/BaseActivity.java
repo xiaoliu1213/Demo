fe677061079541e87d595249bce5c7a3ff7b5817
孟宪光 <hnxxmxg@163.com>
2017-09-20 15:10:14 +0800

    mxg

---
+++ app/src/main/java/com/mxg/gitdemo/base/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.mxg.gitdemo.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
