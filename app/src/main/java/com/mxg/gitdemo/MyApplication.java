865df61e86b6ed99875287c0dc59afa249c02e2a
孟宪光 <hnxxmxg@163.com>
2017-09-20 10:06:04 +0800

    application

---
+++ app/src/main/java/com/mxg/gitdemo/MyApplication.java
@@ -0,0 +1,17 @@
+package com.mxg.gitdemo;
+
+import android.app.Application;
+
+/**
+ * Created by mxg on 2017/9/20.
+ */
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+}
