7ba1b038ec3a2434d097f1448f2e07c543aab4d8
xiaoliu1213 <hnxxmxg@163.com>
2017-09-20 15:18:06 +0800

    Update BaseActivity.java

--- app/src/main/java/com/mxg/gitdemo/base/BaseActivity.java
+++ app/src/main/java/com/mxg/gitdemo/base/BaseActivity.java
@@ -5,8 +5,15 @@ import android.os.Bundle;
 
 public class BaseActivity extends Activity {
 
+    public Activity activity;
+    public Context context;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        activity = this;
+        context = getApplicationContext();
+        
     }
+    
 }
