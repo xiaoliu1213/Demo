4af318b1f325a36bfd3f9999a3567e5bccfcefd7
孟宪光 <hnxxmxg@163.com>
2017-10-11 14:28:29 +0800

    Tag1.0分支

--- app/src/main/java/com/mxg/gitdemo/MainActivity.java
+++ app/src/main/java/com/mxg/gitdemo/MainActivity.java
@@ -13,18 +13,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         LogUtils.e("mxg","Demo");
 
-        // 调试AS的git版本控制
-
-        // 如何提交到master分支上面
-
-        // 这次提交到哪里呢？
-
-        // 再切换到1.0分支上面、
-
-        // 回到正确的轨道上面来、
-
-        // 可以直接更新下来最新的文件吗？ ----> 可以了吗？
-
         // 什么情况？demo1.0
 
     }
